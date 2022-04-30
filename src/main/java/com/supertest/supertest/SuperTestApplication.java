16b1b47605598b394991e26cbae485bd66b66200
umk27 <umk27@mail.ru>
2022-04-30 08:08:32 +0300

    1

---
+++ src/main/java/com/supertest/supertest/SuperTestApplication.java
@@ -0,0 +1,13 @@
+package com.supertest.supertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supertest/supertest/SuperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supertest.supertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
